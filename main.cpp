caee6b864175bdbfd2d445cc6363d3765d6154a4
kdaniel17 <71819952+kdaniel17@users.noreply.github.com>
2022-09-13 15:35:16 +0200

    Update main.cpp
    javítva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N_ElEMENTS
-    std::cout << '1-100 ertekek duplazasa' //; hiánya
-    for (int i = 0;) //nincs megadva az iterálás vége, lépésköze
+    int *b = new int[N_ELEMENTS]; //N_ElEMENTS
+    std::cout << '1-100 ertekek duplazasa'; //; hiánya
+    for (int i = 0; i<N_ELEMENTS; i++) //nincs megadva az iterálás vége, lépésköze
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //egszer fut le
+    for (int i = 0; i<N_ELEMTS; i++) //egszer fut le
     {
-        std::cout << "Ertek:" //; hiánya és nincs érték kiiratás
+        std::cout << "Ertek:" <<b[i]<<std::endl; //; hiánya és nincs érték kiiratás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag; //int helyett double
-    for (int i = 0; i < N_ELEMENTS, i++) //, helyett ;
+    for (int i = 0; i < N_ELEMENTS; i++) //, helyett ;
     {
-        atlag += b[i] //; hiánya
+        atlag += b[i]; //; hiánya
     }
-    atlag /= N_ELEMENTS; //csak osztás kell
+    atlag =atlag / N_ELEMENTS; //csak osztás kell
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
