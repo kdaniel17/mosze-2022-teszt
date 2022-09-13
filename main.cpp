2ccdc8e24b5803c69773ad173093246c76b47535
kdaniel17 <71819952+kdaniel17@users.noreply.github.com>
2022-09-13 15:19:27 +0200

    Update main.cpp
    talán megvan minden

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ElEMENTS
+    std::cout << '1-100 ertekek duplazasa' //; hiánya
+    for (int i = 0;) //nincs megadva az iterálás vége, lépésköze
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //egszer fut le
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //; hiánya és nincs érték kiiratás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //int helyett double
+    for (int i = 0; i < N_ELEMENTS, i++) //, helyett ;
     {
-        atlag += b[i]
+        atlag += b[i] //; hiánya
     }
-    atlag /= N_ELEMENTS;
+    atlag /= N_ELEMENTS; //csak osztás kell
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
